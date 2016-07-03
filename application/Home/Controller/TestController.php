7210ca50745063bf4eb1b52fe4946f31caea2c1e
cheng <18289768853@163.com>
2016-07-03 23:13:13 +0800

    add .htaccess file in the project root directory !

--- application/Home/Controller/TestController.php
+++ application/Home/Controller/TestController.php
@@ -11,10 +11,24 @@ use sys\corePackage\Controller;
 
 class TestController extends Controller
 {
+
+    public function init(){
+        dump('Hello World from init method !');
+    }
+
+
     public function index(){
         echo __METHOD__;
+        $this->assign('colorful' , true);
         $this->assign('key' , ['value0' , 'value1' , 'value3']);
+        $this->assign('obj' , $this);
+//        $this->template->getSmartyObj()->name = 'cheng xiang';
+
         $this->display();
+    }
+
+    public function test(){
+        return "Data from transported object output !";
     }
 
 }
