e1c4c1495d6907971a29267f79f7e0530e2a61f6
helloworld2015cx <18289768853@163.com>
2016-08-08 11:05:42 +0800

    update

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+ *  CSFrame enter here !
+ * */
+
+define( 'ROOT' , __DIR__.'/../');
+
+define('APP_NAME' , 'app');
+
+define('APP_PATH',ROOT.'application/');
+
+
+dump(APP_NAME.'========'.APP_PATH);
+
+require_once(ROOT.'system/autoloader.php');
--- system/corePackage/Model/operate/DB/DBoperate.php
+++ system/corePackage/Model/operate/DB/DBoperate.php
@@ -57,7 +57,7 @@ class DBoperate implements DBoperInterface{
             if($pri){
                 $this->where = ' where '.$pri.'="'.$id.'"';
             }else{
-                error_message(new \Exception('table '.$this->table.'without primary key',10004));
+                error_message(new \Exception('table '.$this->table.' without primary key',10004));
             }
         }else{
             $this->limit(1);
@@ -69,16 +69,26 @@ class DBoperate implements DBoperInterface{
         return $arr[0];
     }
 
+    /**
+     * @return mixed
+     */
     public function first(){
         return $this->find();
     }
 
+    /**
+     * @param $fields
+     * @return $this
+     */
     public function distinct($fields)
     {
         $this->distinctFields = $fields;
         return $this;
     }
 
+    /**
+     * @return null
+     */
     private function get_primary_key(){
         $sql = 'desc '.$this->table;
         $result = $this->get_result($sql);
@@ -90,8 +100,15 @@ class DBoperate implements DBoperInterface{
         return null;
     }
 
-    public function where($field , $compare='=' , $value='' , $connect = ' and '){
-
+    /**
+     * @param $field
+     * @param string $compare
+     * @param string $value
+     * @param string $connect
+     * @return $this
+     */
+    public function where($field , $compare='=' , $value='' , $connect = ' and ')
+    {
         if($field instanceof \Closure){
            $condition = $field()->form_where_conditions();
             $this->where .= $condition;
@@ -114,7 +131,14 @@ class DBoperate implements DBoperInterface{
         return $this;
     }
 
-    public function orWhere($function , $compare = '=' , $value=''){
+    /**
+     * @param $function
+     * @param string $compare
+     * @param string $value
+     * @return $this
+     */
+    public function orWhere($function , $compare = '=' , $value='')
+    {
         if($function instanceof \Closure){
             $condition = $function()->form_orwhere_conditions();
             $this->where.=$condition;
@@ -130,14 +154,26 @@ class DBoperate implements DBoperInterface{
         return $this;
     }
 
-    public function form_orwhere_conditions(){
+    /**
+     * @return string
+     */
+    public function form_orwhere_conditions()
+    {
         return ' or ('.$this->where.')';
     }
 
-    public function form_where_conditions(){
+    /**
+     * @return string
+     */
+    public function form_where_conditions()
+    {
         return ' and ('.$this->where.')';
     }
 
+    /**
+     * @param $field
+     * @return $this
+     */
     public function groupBy($field){
         $this->groupByField = $field;
         return $this;
@@ -147,7 +183,8 @@ class DBoperate implements DBoperInterface{
      * 单个字段排序直接 $field 为字段名，$sort 为排序顺序
      * 多字段排序使用关联数组的方式，键名为字段名，键值为排序顺序
      * */
-    public function orderBy($field,$sort='asc'){
+    public function orderBy($field,$sort='asc')
+    {
         if(is_array($field)){
             $str = '';
             foreach($field as $key=>$value){
@@ -162,23 +199,36 @@ class DBoperate implements DBoperInterface{
         return $this;
     }
 
-    public function limit($start , $num=''){
+    /**
+     * @param $start
+     * @param string $num
+     * @return $this
+     */
+    public function limit($start , $num='')
+    {
         func_num_args()==1 ? : $start.=',' ;
         $this->limit = ' limit '.$start.$num;
         return $this;
     }
 
-    protected function formSql(){
-
+    /**
+     * @return string
+     */
+    protected function formSql()
+    {
         $orderBy = $this->orderByField ? ' order by '.$this->orderByField :'';
-
         $groupBy = $this->groupByField ? ' group by '.$this->groupByField : '';
 
         return 'from'.$this->table.$this->join.$this->where.$groupBy.$orderBy.$this->limit;
     }
 
 
-    private function get_where_delimiter($connect){
+    /**
+     * @param $connect
+     * @return string
+     */
+    private function get_where_delimiter($connect)
+    {
         if(preg_match('/or/i',$connect)){
             return $connect;
         }elseif($connect==STR_NULL){
@@ -189,8 +239,13 @@ class DBoperate implements DBoperInterface{
         }
     }
 
-    public function select($field = '*'){
 
+    /**
+     * @param string $field
+     * @return array|int
+     */
+    public function select($field = '*')
+    {
         if($this->distinctFields && is_array($this->distinctFields)){
             $field = ' distinct '.join(',',$this->distinctFields);
         }elseif($this->distinctFields){
@@ -208,7 +263,8 @@ class DBoperate implements DBoperInterface{
     /*
      * 单条记录更新操作
      * */
-    public function update(array $data){
+    public function update(array $data)
+    {
         $str = ' set ';
         $table = $this->alias?$this->alias:$this->table;
 
@@ -229,8 +285,8 @@ class DBoperate implements DBoperInterface{
      * 传入数值参数，作为主键值进行删除
      * 传入数组参数，数组内的每一个值作为一个主键值进行删除
      * */
-    public function delete($id=''){
-
+    public function delete($id='')
+    {
         if(!$id)
             $sql = 'delete from'.' '.$this->table.' '.$this->where;
         else
@@ -249,34 +305,64 @@ class DBoperate implements DBoperInterface{
      * 一次插入一条记录传递一维数组，
      * 一次插入多条记录传递二维数组
      * */
-    public function insert(array $data){
+    public function insert(array $data)
+    {
         $re = is_array($data) ? $this->form_insert_data($data) : error_message(new Exception(' insert method needs array as parameter',20001));
         $sql = 'insert into'.' '.$this->table.$re;
         $this->get_result($sql);
         return mysqli_insert_id($this->db);
     }
 
-    public function innerJoin($table , $column1 , $column2){
+    /**
+     * @param $table
+     * @param $column1
+     * @param $column2
+     * @return $this
+     */
+    public function innerJoin($table , $column1 , $column2)
+    {
         $this->join($table , $column1 , $column2 , 'inner');
         return $this;
     }
 
+    /**
+     * @param $table
+     * @param $column1
+     * @param $column2
+     * @return $this
+     */
     public function leftJoin($table , $column1 , $column2){
         $this->join($table , $column1 , $column2 , 'left');
         return $this;
     }
 
+    /**
+     * @param $table
+     * @param $column1
+     * @param $column2
+     * @return $this
+     */
     public function rightJoin($table , $column1 , $column2){
         $this->join($table , $column1 , $column2 , 'right');
         return $this;
     }
 
+    /**
+     * @param $table
+     * @param $column1
+     * @param $column2
+     * @param string $direction
+     */
     private function join($table , $column1 , $column2 , $direction='inner'){
         $this->join .= " $direction join ".$table.' on '.$column1.'='.$column2.' ';
     }
 
-    private function form_insert_data(array $data){
-
+    /**
+     * @param array $data
+     * @return string
+     */
+    private function form_insert_data(array $data)
+    {
         $keys =array();
         $values = array();
         $multi = false;
@@ -306,14 +392,24 @@ class DBoperate implements DBoperInterface{
         return $key_str.' values '.$value_str;
     }
 
-    public function get_sql_result($sql){
+
+    /**
+     * @param $sql
+     * @return array|int
+     */
+    public function get_sql_result($sql)
+    {
         $sql = sql_filter($sql);
         return $this->get_result($sql);
 
     }
 
-    private function get_result($sql){
-
+    /**
+     * @param $sql
+     * @return array|int
+     */
+    private function get_result($sql)
+    {
         $this->last_sql = $sql;
 
         try{
@@ -325,19 +421,29 @@ class DBoperate implements DBoperInterface{
 
         $mysqli_result = $this->result;
 
-        while($row = @mysqli_fetch_assoc($mysqli_result)){
-            $result[] = $row;
+        try {
+            while ($row = mysqli_fetch_assoc($mysqli_result)) {
+                $result[] = $row;
+            }
+        }catch (Exception $e){
+            $e->error_trace();
         }
-        @mysqli_free_result($this->result);
+        mysqli_free_result($this->result);
 
         return $result ? $result : mysqli_affected_rows($this->db);
     }
 
+    /**
+     * @return string
+     */
     public function get_last_sql()
     {
         return $this->last_sql;
     }
 
+    /**
+     *
+     */
     public function __destruct(){
         unset($this->db);
     }
